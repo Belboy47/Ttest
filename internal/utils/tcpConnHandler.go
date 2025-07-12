dbfc535333da43290ec84ab52640b941ea414f02
Passenger <155249830+Belboy47@users.noreply.github.com>
2025-07-12 21:14:59 +0330

    Update tcpConnHandler.go

--- internal/utils/tcpConnHandler.go
+++ internal/utils/tcpConnHandler.go
@@ -4,12 +4,16 @@ import (
 	"errors"
 	"io"
 	"net"
+	"syscall"
+	"time"
 
 	"github.com/musix/backhaul/internal/web"
 	"github.com/sirupsen/logrus"
 )
 
 func TCPConnectionHandler(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.Usage, remotePort int, sniffer bool) {
+	setTCPOptions(from, logger)
+	setTCPOptions(to, logger)
 	done := make(chan struct{})
 
 	go func() {
@@ -22,11 +26,9 @@ func TCPConnectionHandler(from net.Conn, to net.Conn, logger *logrus.Logger, usa
 	<-done
 }
 
-// Using direct Read and Write for transferring data
 func transferData(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.Usage, remotePort int, sniffer bool) {
-	buf := make([]byte, 16*1024) // 16K
+	buf := make([]byte, 32*1024) // 32KB buffer for better throughput
 	for {
-		// Read data from the source connection
 		r, err := from.Read(buf)
 		if err != nil {
 			if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) {
@@ -41,7 +43,6 @@ func transferData(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.
 
 		totalWritten := 0
 		for totalWritten < r {
-			// Write data to the destination connection
 			w, err := to.Write(buf[totalWritten:r])
 			if err != nil {
 				if errors.Is(err, net.ErrClosed) {
@@ -52,15 +53,44 @@ func transferData(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.
 				from.Close()
 				to.Close()
 				return
-
 			}
 			totalWritten += w
 		}
 
-		logger.Tracef("read data: %d bytes, written data: %d bytes", r, totalWritten)
+		logger.Tracef("read %d bytes, wrote %d bytes", r, totalWritten)
 		if sniffer {
 			usage.AddOrUpdatePort(remotePort, uint64(totalWritten))
 		}
 	}
+}
 
+func setTCPOptions(conn net.Conn, logger *logrus.Logger) {
+	tcpConn, ok := conn.(*net.TCPConn)
+	if !ok {
+		return
+	}
+
+	if err := tcpConn.SetKeepAlive(true); err != nil {
+		logger.Debug("failed to enable SO_KEEPALIVE: ", err)
+	}
+	if err := tcpConn.SetKeepAlivePeriod(45 * time.Second); err != nil {
+		logger.Debug("failed to set keepalive period: ", err)
+	}
+	if err := tcpConn.SetNoDelay(true); err != nil {
+		logger.Debug("failed to enable TCP_NODELAY: ", err)
+	}
+
+	rawConn, err := tcpConn.SyscallConn()
+	if err != nil {
+		logger.Debug("failed to get syscall connection: ", err)
+		return
+	}
+
+	rawConn.Control(func(fd uintptr) {
+		linger := syscall.Linger{
+			Onoff:  1,
+			Linger: 2,
+		}
+		syscall.SetsockoptLinger(int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER, &linger)
+	})
 }
